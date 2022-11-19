22b4326742896b106a0efec38abe153bab4133b8
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-19 22:16:22 +0530

    Delegates and Multicast Delegates

---
+++ Delegates_And_Evants/Delegates_And_Evants/MulticastDelegate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_And_Evants
+{
+    public class MulticastDelegates
+    {
+        delegate void MultiOperation(int x, int y);
+
+        public static void ImplementDelegate()
+        {
+            Console.WriteLine("\n==========Multicast Delegates==========");
+            MultiOperation objMulti = Operation.Add;
+            objMulti += Operation.Sub;
+
+            objMulti(10, 5);
+            objMulti(15, 20);
+        }
+
+        public class Operation
+        {
+            public static void Add(int a, int b)
+            {
+                Console.WriteLine($"\nAddition: {a + b}");
+            }
+
+            public static void Sub(int a, int b)
+            {
+                Console.WriteLine($"\nSubtraction: {a - b}");
+            }
+        }
+    }
+}
---
+++ Delegates_And_Evants/Delegates_And_Evants/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Delegates_And_Evants
+{
+    public delegate int Operation(int x, int y);
+    public class Program
+    {
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Delegates\n");
+            Operation obj1 = new Operation(Addition);
+            Operation obj2 = new Operation(Subtraction);
+
+            Console.WriteLine($"\nAddition of two numbers is: {obj1(20, 30)}");
+            Console.WriteLine($"\nSubtraction of two numbers is: {obj2(20, 30)}");
+
+            MulticastDelegates.ImplementDelegate();
+
+            Console.ReadLine();
+        }
+    }
+}
